972602abf2473df896ad4ea69d04c24056d67481
sefagvneri <sefaguneri1907@gmail.com>
2023-07-11 15:04:09 +0300

    Basic Exercises.

---
+++ ArtıkYıl/src/App.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner sc  = new Scanner(System.in);
+
+        int year;
+
+        System.out.println("Enter the year: ");
+        year = sc.nextInt();
+
+        if(year % 4 == 0 && year % 400 == 0) {
+            System.out.println(year + " is a leap year.");
+        }
+        else {
+            System.out.println(year + " is not a leap year.");
+        }
+
+    
+    }
+}
