da9cbaf59c0703b68725ffafbfb82ffd547ec033
sefagvneri <sefaguneri1907@gmail.com>
2023-07-16 20:37:27 +0300

    Basic Exercises

---
+++ EBOBveEKOK/src/App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int n1, n2;
+        int ebob = 1;
+        int ekok;
+        System.out.println("Enter the n1: ");
+        n1 = sc.nextInt();
+        System.out.println("Enter the n2: ");
+        n2 = sc.nextInt();
+
+        for(int i=n1; i>=1; i--){
+            if(n1 % i == 0 && n2 % i == 0){
+                ebob = i;
+                System.out.println("EBOB of n1 and n2 : " + i);
+                ekok = (n1*n2)/ebob;
+                System.out.println("EKOK of n1 and n2 : " + ekok);
+                break;
+            }
+        }
+    }
+}
